45ba6fa94634ef64e724b33a113974ecdebe40b3
Justin Parker <jparkerweb@gmail.com>
2025-01-10 12:26:26 -0800

    rename vault

--- scripts/copy-build.mjs
+++ scripts/copy-build.mjs
@@ -1,7 +1,7 @@
 import fs from 'fs-extra';
 import path from 'path';
 
-const targetDir = 'example-vault/rich-foot-example/.obsidian/plugins/rich-foot';
+const targetDir = '.vault/rich-foot-example/.obsidian/plugins/rich-foot';
 const filesToCopy = ['styles.css', 'main.js', 'manifest.json'];
 
 // Ensure target directory exists
